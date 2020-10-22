d3c0ec89182ea415ed1e933da14f00723f09c937
Seaenna <58196475+Seaenna@users.noreply.github.com>
2020-10-22 15:15:54 -0400

    lab 6

--- public/lab_6/script.js
+++ public/lab_6/script.js
@@ -1,5 +1,7 @@
 // You may wish to find an effective randomizer function on MDN.
 
+//const { default: countries } = require("./countries");
+
 function range(int) {
   const arr = [];
   for (let i = 0; i < int; i += 1) {
@@ -17,6 +19,13 @@ function sortFunction(a, b, key) {
   return 0;
 }
 
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive 
+}
+
+
 document.body.addEventListener('submit', async (e) => {
   e.preventDefault(); // this stops whatever the browser wanted to do itself.
   const form = $(e.target).serializeArray(); // here we're using jQuery to serialize the form
@@ -29,8 +38,26 @@ document.body.addEventListener('submit', async (e) => {
   })
     .then((fromServer) => fromServer.json())
     .then((fromServer) => {
-      // You're going to do your lab work in here. Replace this comment.
-      console.log('fromServer', fromServer);
+      if (document.querySelector('.flex-inner')) {
+        document.querySelector('.flex-inner').remove();
+      }
+      const rangeCountries = range(10);
+      const randomCountries = rangeCountries.map(() => {
+        const randomNumber = getRandomIntInclusive(0, 243);
+        return fromServer[randomNumber];
+      });
+
+      const reverseAlph = randomCountries.sort((a, b) => sortFunction(b, a, 'name'));
+      const ol = document.createElement('ol');
+      ol.className = 'flex-inner';
+      $('form').prepend(ol);
+
+      reverseAlph.forEach((el, i) => {
+        const li = document.createElement('li');
+        $(li).append(`<input type="checkbox" value=${el.code} id=${el.code} />`);
+        $(li).append(`<label for=${el.code}>${el.name}</label>`);
+        $(ol).append(li);
+      });
     })
     .catch((err) => console.log(err));
 });
